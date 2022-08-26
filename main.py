57d3dad111f587a3645811a664cc887e60d0ee0b
Akhil Kondepudi <junnu02@gmail.com>
2022-08-26 23:39:55 +0000

    Added functionality to join and leave voice call

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import discord
+from discord.ext import commands
+import requests
 import os
 
-client = discord.Client()
+
+intents = discord.Intents.all()
+intents.members = True
+
+client = commands.Bot(command_prefix = '!', intents=intents)
 
 @client.event
 async def on_ready():
   print("we have launched as {0.user}".format(client))
 
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
+@client.command(pass_context = True)
+async def tester(ctx):
+  await ctx.send("hello, its working poggo")
 
-  if message.content.startswith("$hello"):
-    await message.channel.send("Hello")
+@client.command(pass_context = True)
+async def join(ctx):
+  if (ctx.author.voice):
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+  else:
+    await ctx.send("You are not in voice channel sadge")
+
+@client.command(pass_context = True)
+async def leave(ctx):
+  if (ctx.voice_client):
+    await ctx.guild.voice_client.disconnect()
+  else:
+    await ctx.send("Im not in the voice channel you big dumbo")
 
 client.run(os.environ['TOKEN'])
